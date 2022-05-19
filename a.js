93f0f8f944c72049b2f10ee716b3a4640b7eda54
Tien Nguyen Tran <86861719+LynnGG@users.noreply.github.com>
2022-05-19 11:39:16 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,2 @@
-document.evaluate("//*[@id='inner-column-container']/div[2]/div/h3/b", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent = 'Lynn';
 alert("abc");
+document.evaluate("//*[@id='inner-column-container']/div[2]/div/h3/b", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent = 'Lynn';
