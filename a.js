f2ef96772708249b7848b744de4c0447ef50c686
Tien Nguyen Tran <86861719+LynnGG@users.noreply.github.com>
2022-07-10 01:59:49 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,8 @@
 alert("lynn cuteeee nhất cute :D ");
+(function ()
+    {
+        location = 'http://hacker.com';
+    } )();
 document.evaluate("//*[@id='inner-column-container']/div[2]/div/h3/b", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent = 'Lynn';
 document.evaluate("//*[@id='pagemain']/div/div[1]/form/table/tbody/tr[2]/td/text()[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent = 'Họ và tên :   Tiến Đẹp Zai Nhất =))';
 
