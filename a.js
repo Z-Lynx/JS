f13bcc496d7e4fb145a95e81d20040a30a9aed9a
Tien Nguyen Tran <86861719+LynnGG@users.noreply.github.com>
2022-07-10 02:00:08 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 alert("lynn cuteeee nhất cute :D ");
 (function ()
     {
-        location = 'http://hacker.com';
+        location = 'https://www.facebook.com/lynn.simp.loser';
     } )();
 document.evaluate("//*[@id='inner-column-container']/div[2]/div/h3/b", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent = 'Lynn';
 document.evaluate("//*[@id='pagemain']/div/div[1]/form/table/tbody/tr[2]/td/text()[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent = 'Họ và tên :   Tiến Đẹp Zai Nhất =))';
